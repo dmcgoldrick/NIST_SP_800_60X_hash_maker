ee2c73788a3335dba1edf4a30eb3a1977f023b0e
mcgold <mcgoldphd@gmail.com>
2025-09-01 09:30:01 -0700

    fix cli typo

--- NIST_SP_800_63X_hash_maker/cli.py
+++ NIST_SP_800_63X_hash_maker/cli.py
@@ -7,7 +7,7 @@ import argparse
 from NIST_SP_800_63X_hash_maker.hashing import (
     normalize_secret,
     ARGON2_PARAMS,
-    PBKDF2_TIERS,
+    PBKDF2_ITERS,
     hash_all,
     benchmark_argon2id,
     verify_argon2id,
